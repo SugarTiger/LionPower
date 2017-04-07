d4ad4009eb6685bc441fc7d181d62372ee7a143d
SugarTiger <1029109173@qq.com>
2017-04-07 22:12:54 +0800

    增加选显卡功能

--- js/public.js
+++ js/public.js
@@ -1,10 +1,10 @@
-$(function () {
+$(function() {
     // 轮播图js
     // 添加焦点和左右箭头
     function addOlSpan(bannerId) {
         var liImgs = $("#" + bannerId).find("ul").find("li");
         var ol = $("<ol></ol>");
-        liImgs.each(function (i) {
+        liImgs.each(function(i) {
             ol.append($("<li/>"));
         });
         var lspan = '<span id="l"></span>';
@@ -14,10 +14,12 @@ $(function () {
         $("#l").hide();
         $("#r").hide();
     };
+
     function position(bannerId, imgH, imgW) {
         var poddingVal = parseInt(imgH) / parseInt(imgW) * 100 + "%";
         $("#" + bannerId).find("ul").css("padding-bottom", poddingVal);
     }
+
     function active() {
         $("#banner ol li").eq(i).addClass("active").siblings().removeClass("active");
         $("#banner ul li").eq(i).fadeIn(800).siblings().fadeOut(800);
@@ -43,40 +45,48 @@ $(function () {
     var i = 0;
     var size = $("#banner ol li").length;
     // 绑定左右控制
-    $("#l").click(function () {
+    $("#l").click(function() {
         moveL();
     });
-    $("#r").click(function () {
+    $("#r").click(function() {
         moveR();
     });
     // 自动轮播
     var autoMove = setInterval(moveR, 3000);
-    $("#banner").hover(function () {
+    $("#banner").hover(function() {
         clearInterval(autoMove);
         $("#l").show();
         $("#r").show();
-    }, function () {
+    }, function() {
         autoMove = setInterval(moveR, 3000);
         $("#l").hide();
         $("#r").hide();
     });
     // 焦点点击
-    $("#banner ol li").each(function () {
-        $(this).click(function () {
+    $("#banner ol li").each(function() {
+        $(this).click(function() {
             i = $(this).index();
             active();
         });
     });
 
     // 导航条的固定标签
-    function navAddClass(){
+    (function navAddClass() {
         var url = location.href;
-        $("#nav li a").each(function(){
+        $("#nav li a").each(function() {
             var aUrl = $(this).attr("href");
-            if(url.indexOf(aUrl) != -1){
+            if (url.indexOf(aUrl) != -1) {
                 $(this).addClass("on");
             }
         });
-    };
-    navAddClass();
+    })();
+    // 左边选项卡
+    (function() {
+        $(".leftNav ul li").each(function() {
+            $(this).click(function() {
+                $(this).addClass("active").siblings().removeClass("active");
+                $(".rightMain > div").eq($(this).index()).css("display", "block").siblings().css("display", "none");
+            });
+        });
+    })();
 });
